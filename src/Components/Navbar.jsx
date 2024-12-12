f747f83ed90c476fd3a39c5a923cadbbd8857d2b
j4son <alderson.j4son@gmail.com>
2024-12-12 17:15:05 +0330

    add navbar component and change some css for starting

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './NavBar.css'
+
+export default function NavBar(){
+
+    return(
+        <>
+           <nav>
+            <div className="navItems-left">
+                
+            </div>
+            <div className="navItems-right">
+
+            </div>
+           </nav>
+        </>
+    )
+}
