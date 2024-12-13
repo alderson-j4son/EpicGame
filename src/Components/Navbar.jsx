ba83fab0f17b14e49d9620dc7b0a463e00b70dd4
j4son <alderson.j4son@gmail.com>
2024-12-14 03:13:04 +0330

    from header navBar style is done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './NavBar.css'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import LanguageIcon from '@mui/icons-material/Language';
 
 export default function NavBar(){
 
@@ -7,10 +9,20 @@ export default function NavBar(){
         <>
            <nav>
             <div className="navItems-left">
-                
+                <div className="logo">
+                    <a href="">
+                        <img src="https://static-assets-prod.epicgames.com/epic-store/static/webpack/../favicon.ico" alt="" />
+                            <KeyboardArrowDownIcon/>
+                    </a>
+                </div>
+                <a className="active" href=""><img src="https://cms-assets.unrealengine.com/qAiDvosPSFGqJxTVuY7h" alt="" /></a>
+                <a href="">Support</a>
+                <a className="drop-down" href="">Distribute<KeyboardArrowDownIcon/></a>
             </div>
             <div className="navItems-right">
-
+                <a href=""><LanguageIcon/></a>
+                <a href="">Sign in</a>
+                <a href="">Download</a>
             </div>
            </nav>
         </>
